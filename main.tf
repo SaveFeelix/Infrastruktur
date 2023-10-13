639a5e8543ac770016ee5aa6ff2a70e2ca1a4a69
SaveFeelix <felixhake@web.de>
2023-10-13 17:13:50 +0200

    Fix CI-error

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
+  required_version = ">=1.6.1"
   required_providers {
     docker = {
-      source = "kreuzwerker/docker"
-      version = "3.0.2"
+      source  = "kreuzwerker/docker"
     }
   }
 }
